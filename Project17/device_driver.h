198115eccd163511426baa1b50fc09fedc6ab548
ywsin.kim <ywsin.kim@samsung.com>
2025-07-24 16:21:19 +0900

    step 1 - driver

--- Project17/device_driver.h
+++ Project17/device_driver.h
@@ -1,5 +1,20 @@
 #pragma once
 #include "flash_memory_device.h"
+#include "stdexcept"
+
+class ReadFailException : public std::runtime_error {
+public:
+    // 생성자: 오류 메시지를 전달받음
+    explicit ReadFailException(const std::string& message)
+        : std::runtime_error("Read failed: " + message) {}
+};
+
+class WriteFailException : public std::runtime_error {
+public:
+    // 생성자: 오류 메시지를 전달받음
+    explicit WriteFailException(const std::string& message)
+        : std::runtime_error("write failed: " + message) {}
+};
 
 class DeviceDriver
 {
--- Project17/main.cpp
+++ Project17/main.cpp
@@ -1,13 +1,81 @@
-#include "gmock/gmock.h"
+﻿#include "gmock/gmock.h"
 #include "device_driver.h"
+#include "flash_memory_device.h"
 
-TEST(DeviceDriver, ReadFromHW) {
-	// TODO : replace hardware with a Test Double
-	FlashMemoryDevice* hardware = nullptr;
-	DeviceDriver driver{ hardware };
-	int data = driver.read(0xFF);
-	EXPECT_EQ(0, data);
+
+/*
+Device Driver, Read 기능 개발 명세서
+▪ Read를5회수행한다.
+▪ 5회의Read의결과가모두같은값일때, 읽은값을return 한다.
+ • 모두같은값이아니라면, Custom Exception(ReadFailException)을 발생시킨다.
+
+
+ Device Driver, Write 기능 개발 명세서
+▪ 해당객체에이미값이적혀있는지확인한다.
+ (읽었을때0xFF라면, 값이지워진상태이다.)
+ ▪ 값이적혀있지않을때, 해당주소에값을Write한다.
+ • 이미값이적혀있다면Custom Exception(WriteFailException)을 발생시킨다.
+*/
+using namespace testing;
+class MockFlash : public FlashMemoryDevice
+{
+public:
+	MOCK_METHOD(unsigned char, read, (long), (override));
+	MOCK_METHOD(void, write, (long, unsigned char), (override));
+};
+
+
+class fixtureFlash : public testing::Test
+{
+
+
+protected :
+	void SetUp(void) override
+	{
+		driver = new DeviceDriver(&mock);
+	}
+public:
+	NiceMock<MockFlash> mock;
+	DeviceDriver* driver;
+
+};
+
+
+TEST_F(fixtureFlash, ReadFromHW) {
+
+	EXPECT_CALL(mock, read((long)0xB))
+		.Times(5);
+
+	int ret = driver->read(0xB);
 }
+
+TEST_F(fixtureFlash, WriteFromHW) {
+	EXPECT_CALL(mock, read)
+		.WillRepeatedly(testing::Return(0xFF));
+	EXPECT_CALL(mock, write)
+		.Times(1);
+
+	driver->write(0, 0x8);
+
+}
+
+
+TEST_F(fixtureFlash, ReadFromHWAndException) {
+	EXPECT_CALL(mock, read((long)0xB))
+		.WillOnce(Return((unsigned char)0xA))
+		.WillOnce(Return((unsigned char)0xA))
+		.WillOnce(Return((unsigned char)0xB));
+	EXPECT_THROW(driver->read(0xB), ReadFailException);
+}
+
+TEST_F(fixtureFlash, WriteFromHWAndException) {
+	EXPECT_CALL(mock, read((long)0xB))
+		.WillRepeatedly(Return((unsigned char)0xAA));
+
+	EXPECT_THROW(driver->write(0xB, 0xBB), WriteFailException);
+}
+
+
 
 int main() {
 	::testing::InitGoogleMock();
